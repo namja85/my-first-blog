093989fbc73f760b5254e00f35e7e7e33b7530b1
namja85 <namja85@gmail.com>
2020-05-20 14:05:34 +0900

    Added views create/edit blog post inside the site.

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
+    objects = models.Manager()
+    
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
